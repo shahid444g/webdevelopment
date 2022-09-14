f2d9483a0953ea57014d27b9bb7793c85da725ea
shaik shoaib <futuretech9966@gmail.com>
2022-09-14 20:36:58 +0530

    web

---
+++ day1.js
@@ -0,0 +1,20 @@
+let number = prompt("enter your number");
+Number.parseInt(number);
+if (number == 9966789024) {
+    
+let string1 = prompt("enter your password");
+let string2 = "shahid"
+const result = string1.toUpperCase() === string2.toUpperCase();
+if (result) {
+    document.getElementById("test").style="display:flex";
+
+}
+else{
+    document.write("wrong password")
+}
+
+}
+else{
+    document.write("wrong number you cannot even think of visiting our page try again");
+    
+}
